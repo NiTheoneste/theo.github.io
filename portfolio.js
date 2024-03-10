ec3c7d67930f819c4afab17abae314f9ac679edf
Théoneste Nizerimana <162795638+NiTheoneste@users.noreply.github.com>
2024-03-10 10:15:19 +0200

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialisation de Typed.js
+    const typed = new Typed('.multiple-text', {
+        strings: ['FullStack Developer', 'Video Editor', 'Hucker Engineer', 'Motion and Graphics Designer'],
+        typeSpeed: 70,
+        backSpeed: 70,
+        backDelay: 1000,
+        loop: true,
+    });
+
+    // Initialisation de ScrollReveal
+    ScrollReveal({
+        distance: '80px',
+        duration: 2000,
+        delay: 200,
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-contact h1, .about-image', { origin: 'left' });
+    ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+});
+
+let menuIcon = document.querySelector('.fa-bars'); // Modifier la sélection ici
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
